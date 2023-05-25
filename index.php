000dbf08b2e086b1d033b4667eb3e1f5e9bb9364
Yash Bhayani <83011938+Yashbhayani@users.noreply.github.com>
2023-05-25 17:35:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,6 @@ header('Content-type:application/json');
 
 
 include 'DbConnect.php';
-// $objDb = new DbConnect;
-// $conn = $objDb->con();
 
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
@@ -17,8 +15,7 @@ $uri = $_SERVER['REQUEST_URI'];
 switch ($method) {
     case "GET":
         /*      $headers = apache_request_headers();
-                $token = $headers['auth-token'];
-                echo $token;*/
+                $token = $headers['auth-token']; */
 
         $sql = "SELECT * FROM `users`";
         $query = mysqli_query($con, $sql);
@@ -73,88 +70,4 @@ switch ($method) {
         echo json_encode($response);
         return 0;
 }
-
-/*switch ($method) {
-    case "GET":
-        $sql = "SELECT * FROM users";
-        $path = explode('/', $_SERVER['REQUEST_URI']);
-        if (isset($path[3]) && is_numeric($path[3])) {
-            $sql .= " WHERE id = :id";
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':id', $path[3]);
-            if ($stmt->execute()) {
-                $response = ['status' => 1, 'message' => 'Record created successfully.'];
-            } else {
-                $response = ['status' => 0, 'message' => 'Failed to create record.'];
-            }
-            $users = $stmt->fetch(PDO::FETCH_ASSOC);
-        } else {
-            $stmt = $conn->prepare($sql);
-            if ($stmt->execute()) {
-                $response = ['status' => 1, 'message' => 'Record created successfully.'];
-            } else {
-                $response = ['status' => 0, 'message' => 'Failed to create record.'];
-            }
-            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
-
-        // $headers = apache_request_headers();
-        // $token = $headers['auth-token'];
-        $data = [
-            $users, $response
-        ];
-        echo json_encode($users);
-        break;
-    case "POST":
-        $user = json_decode(file_get_contents('php://input'));
-        $sql = "INSERT INTO users(id, name, email, mobile, created_at) VALUES(null, :name, :email, :mobile, :created_at)";
-        $stmt = $conn->prepare($sql);
-        $created_at = date('Y-m-d');
-        $stmt->bindParam(':name', $user->name);
-        $stmt->bindParam(':email', $user->email);
-        $stmt->bindParam(':mobile', $user->mobile);
-        $stmt->bindParam(':created_at', $created_at);
-
-        if ($stmt->execute()) {
-            $response = ['status' => 1, 'message' => 'Record created successfully.'];
-        } else {
-            $response = ['status' => 0, 'message' => 'Failed to create record.'];
-        }
-        echo json_encode($response);
-        break;
-
-    case "PUT":
-        $user = json_decode(file_get_contents('php://input'));
-        $sql = "UPDATE users SET name= :name, email =:email, mobile =:mobile, updated_at =:updated_at WHERE id = :id";
-        $stmt = $conn->prepare($sql);
-        $updated_at = date('Y-m-d');
-        $stmt->bindParam(':id', $user->id);
-        $stmt->bindParam(':name', $user->name);
-        $stmt->bindParam(':email', $user->email);
-        $stmt->bindParam(':mobile', $user->mobile);
-        $stmt->bindParam(':updated_at', $updated_at);
-
-        if ($stmt->execute()) {
-            $response = ['status' => 1, 'message' => 'Record updated successfully.'];
-        } else {
-            $response = ['status' => 0, 'message' => 'Failed to update record.'];
-        }
-        echo json_encode($response);
-        break;
-
-    case "DELETE":
-        $sql = "DELETE FROM users WHERE id = :id";
-        $path = explode('/', $_SERVER['REQUEST_URI']);
-
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':id', $path[3]);
-
-        if ($stmt->execute()) {
-            $response = ['status' => 1, 'message' => 'Record deleted successfully.'];
-        } else {
-            $response = ['status' => 0, 'message' => 'Failed to delete record.'];
-        }
-        echo json_encode($response);
-        break;
-    }
-*/
+?>
